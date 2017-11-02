3c83582e8b92f701d3248de2a9b62b053d72ac18
Chris James <qui666@gmail.com>
2017-11-02 09:37:13 +0000

    refactoring away stupid

--- test/secret_santa/quick_check_test.clj
+++ test/secret_santa/quick_check_test.clj
@@ -7,27 +7,21 @@
             [clojure.test.check.properties :as prop]
             ))
 
-(defn get-givers [givers-and-receivers] (map first givers-and-receivers))
-(defn get-receivers [givers-and-receivers] (map second givers-and-receivers))
-
 (defn check-santas-appear-once [original-santas assigned-santas]
   (let [appeared-once (distinct? assigned-santas)
-        all-appeared (= (set assigned-santas) (set original-santas))
-        ]
+        all-appeared (= (set assigned-santas) (set original-santas))]
       (and all-appeared appeared-once)
     ))
 
 (def every-santa-gives-and-receives-once
   (prop/for-all [santas (gen/vector gen/int)]
                 (let [assignments (assign-giving-and-receiving santas)
-                      givers (get-givers assignments)
-                      receivers (get-receivers assignments)
+                      givers (keys assignments)
+                      receivers (vals assignments)
                       ]
                   (true? (check-santas-appear-once santas givers))
                   (true? (check-santas-appear-once santas receivers))
                   )
                 ))
-
-
 
 (defspec santa-gives-once 100 every-santa-gives-and-receives-once)
